bbe7d79e63d1249ac47e463ad922fe959064ee87
Daniel Chu <mrdandude321@gmail.com>
2025-08-05 11:45:32 -0700

    Create DonutChart.tsx

---
+++ code/DonutChart.tsx
@@ -0,0 +1,212 @@
+import svgPaths from "./svg-5s4xvik11u";
+
+function Group2() {
+  return (
+    <div className="absolute contents left-[26px] top-2">
+      <div className="absolute left-[26px] size-[126px] top-2">
+        <svg
+          className="block size-full"
+          fill="none"
+          preserveAspectRatio="none"
+          viewBox="0 0 126 126"
+        >
+          <path
+            d={svgPaths.p3e456b00}
+            fill="var(--fill-0, #FFAFAF)"
+            id="Ellipse 1"
+          />
+        </svg>
+      </div>
+      <div className="absolute left-[26px] size-[126px] top-2">
+        <svg
+          className="block size-full"
+          fill="none"
+          preserveAspectRatio="none"
+          viewBox="0 0 126 126"
+        >
+          <path
+            d={svgPaths.p1d9cf1c0}
+            fill="var(--fill-0, #FF8282)"
+            id="Ellipse 4"
+          />
+        </svg>
+      </div>
+      <div className="absolute left-[26px] size-[126px] top-2">
+        <svg
+          className="block size-full"
+          fill="none"
+          preserveAspectRatio="none"
+          viewBox="0 0 126 126"
+        >
+          <path
+            d={svgPaths.p17821200}
+            fill="var(--fill-0, #FF5959)"
+            id="Ellipse 3"
+          />
+        </svg>
+      </div>
+      <div className="absolute left-[26px] size-[126px] top-2">
+        <div className="absolute bottom-0 left-0 right-1/2 top-0">
+          <svg
+            className="block size-full"
+            fill="none"
+            preserveAspectRatio="none"
+            viewBox="0 0 63 126"
+          >
+            <path
+              d={svgPaths.p3fa1c00}
+              fill="var(--fill-0, #FF3131)"
+              id="Ellipse 2"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default function DonutChart() {
+  return (
+    <div className="relative size-full" data-name="Donut Chart">
+      <div
+        className="absolute bottom-[5.97%] left-[11.78%] right-[11.9%] top-[2.03%]"
+        data-name="Vector"
+      >
+        <div className="absolute inset-[-0.092%]">
+          <svg
+            className="block size-full"
+            fill="none"
+            preserveAspectRatio="none"
+            viewBox="0 0 138 138"
+          >
+            <path
+              d={svgPaths.p6e6ee00}
+              id="Vector"
+              stroke="var(--stroke-0, #A28888)"
+              strokeMiterlimit="10"
+              strokeWidth="0.25"
+            />
+          </svg>
+        </div>
+      </div>
+      <div
+        className="absolute bottom-[24.74%] left-[26.93%] right-[27.48%] top-[20.29%]"
+        data-name="Vector"
+      >
+        <div className="absolute inset-[-0.154%]">
+          <svg
+            className="block size-full"
+            fill="none"
+            preserveAspectRatio="none"
+            viewBox="0 0 83 83"
+          >
+            <path
+              d={svgPaths.p94d7900}
+              id="Vector"
+              stroke="var(--stroke-0, #A28888)"
+              strokeMiterlimit="10"
+              strokeWidth="0.25"
+            />
+          </svg>
+        </div>
+      </div>
+      <div
+        className="absolute bottom-[81.91%] left-[78.89%] right-0 top-[8.33%]"
+        data-name="Vector"
+      >
+        <div className="absolute bottom-[-0.61%] left-[-0.23%] right-0 top-[-0.87%]">
+          <svg
+            className="block size-full"
+            fill="none"
+            preserveAspectRatio="none"
+            viewBox="0 0 39 16"
+          >
+            <path
+              d="M1 15.4432L15.4432 1H38.6301"
+              id="Vector"
+              stroke="var(--stroke-0, #A28888)"
+              strokeMiterlimit="10"
+              strokeWidth="0.25"
+            />
+          </svg>
+        </div>
+      </div>
+      <div
+        className="absolute bottom-[23.64%] left-[82.22%] right-[2.45%] top-[72.07%]"
+        data-name="Vector"
+      >
+        <div className="absolute bottom-[-1.97%] left-[-0.32%] right-0 top-[-1.39%]">
+          <svg
+            className="block size-full"
+            fill="none"
+            preserveAspectRatio="none"
+            viewBox="0 0 29 8"
+          >
+            <path
+              d="M1 1L7.34473 7.3446H28.3152"
+              id="Vector"
+              stroke="var(--stroke-0, #A28888)"
+              strokeMiterlimit="10"
+              strokeWidth="0.25"
+            />
+          </svg>
+        </div>
+      </div>
+      <div
+        className="absolute bottom-0 left-[61.13%] right-[21.01%] top-[91.76%]"
+        data-name="Vector"
+      >
+        <div className="absolute bottom-[-1.03%] left-[-0.32%] right-0 top-[-0.58%]">
+          <svg
+            className="block size-full"
+            fill="none"
+            preserveAspectRatio="none"
+            viewBox="0 0 33 14"
+          >
+            <path
+              d="M1 1L9.32291 13.1836H32.8508"
+              id="Vector"
+              stroke="var(--stroke-0, #A28888)"
+              strokeMiterlimit="10"
+              strokeWidth="0.25"
+            />
+          </svg>
+        </div>
+      </div>
+      <div
+        className="absolute bottom-[72.23%] left-0 right-[84.55%] top-[22.29%]"
+        data-name="Vector"
+      >
+        <div className="absolute bottom-[-1.09%] left-0 right-[-0.32%] top-[-1.54%]">
+          <svg
+            className="block size-full"
+            fill="none"
+            preserveAspectRatio="none"
+            viewBox="0 0 28 10"
+          >
+            <path
+              d="M27.5365 9.1004L19.4361 1H0"
+              id="Vector"
+              stroke="var(--stroke-0, #A28888)"
+              strokeMiterlimit="10"
+              strokeWidth="0.25"
+            />
+          </svg>
+        </div>
+      </div>
+      <div className="absolute bottom-[79.04%] font-['Arial:Regular',_sans-serif] leading-[0] left-[1.23%] not-italic right-[89.24%] text-[#ffffff] text-[8.281px] text-left top-[14.19%]">
+        <p className="block leading-[normal]">50%</p>
+      </div>
+      <div className="absolute bottom-[93.24%] font-['Arial:Regular',_sans-serif] leading-[0] left-[88.99%] not-italic right-[1.48%] text-[#ffffff] text-[8.281px] text-left top-0">
+        <p className="block leading-[normal]">25%</p>
+      </div>
+      <div className="absolute bottom-[24.86%] font-['Arial:Regular',_sans-serif] leading-[0] left-[87.3%] not-italic right-[3.17%] text-[#ffffff] text-[8.281px] text-left top-[68.38%]">
+        <p className="block leading-[normal]">18%</p>
+      </div>
+      <div className="absolute bottom-[1.1%] font-['Arial:Regular',_sans-serif] leading-[0] left-[67.41%] not-italic right-[23.06%] text-[#ffffff] text-[8.281px] text-left top-[92.13%]">
+        <p className="block leading-[normal]">10%</p>
+      </div>
+      <Group2 />
+    </div>
+  );
+}
